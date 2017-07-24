538ad3f9a677ad159dc09607088bef5fd1866952
Лугин Евгений <lugin@tutu.ru>
2017-07-24 16:54:28 +0300

    Merge pull request #6 in ~NIKULIN/php-lugin from TASK-1.5-sort_num_percent to master
    Squashed commit of the following:
    
    commit 2df694e9e72c91c0fd28fbd57eb6f2e819b3d7ff
    Author: lugin <lugin@tutu.ru>
    Date:   Thu Jul 20 10:04:41 2017 +0300
    
        fix
    
    commit 19990bb5f1cff7ea98c1c044aecff243f4aae93a
    Author: lugin <lugin@tutu.ru>
    Date:   Wed Jul 19 09:41:30 2017 +0300
    
        fix some
    
    commit 65405de8b8be19767ad5875c4719b66beedd73db
    Author: lugin <lugin@tutu.ru>
    Date:   Mon Jul 17 16:06:13 2017 +0300
    
        task 5

--- numberSorter.php
+++ numberSorter.php
@@ -7,39 +7,63 @@
  * в порядке возрастания
  * в порядке убывания
  *
+ * К программе из п. 4 добавить вывод доли (в %) каждого числа от суммы всех введенных чисел. Дробные числа округлять до 2 знаков после запятой
+ *
  */
 
 $userNumbers = array_slice($argv,1);
-if (!isOnlyIntegers($userNumbers))
+if (!containsOnlyIntegers($userNumbers))
 {
     echo "Не введены подходящие аргументы. Пример: php numberSorter.php 1 2 3 4 5 6" . PHP_EOL;
-}
-else
-{
-    $reversedStr = implode(" ", array_reverse($userNumbers));
-    asort($userNumbers);
-    $increasingStr = implode(" ", $userNumbers);
-    arsort($userNumbers);
-    $decreasingStr = implode(" ", $userNumbers);
-
-    echo "Обратный порядок: " . $reversedStr . PHP_EOL;
-    echo "По возрастанию: " . $increasingStr . PHP_EOL;
-    echo "По убыванию: " . $decreasingStr . PHP_EOL;
+    exit;
 }
 
+$reversedString = implode(" ", array_reverse($userNumbers));
+asort($userNumbers);
+$increasingString = implode(" ", $userNumbers);
+arsort($userNumbers);
+$decreasingString = implode(" ", $userNumbers);
 
-function isOnlyIntegers($arr)
+echo "Обратный порядок: " . $reversedString . PHP_EOL;
+echo "По возрастанию: " . $increasingString . PHP_EOL;
+echo "По убыванию: " . $decreasingString . PHP_EOL;
+
+$numbersSum = array_sum($userNumbers);
+if ($numbersSum <= 0)
 {
-    if (!$arr)
+    echo "Сумма всех элементов меньше или равна нулю. Корректно посчитать процент каждого числа от суммы всех введенных чисел нельзя" . PHP_EOL;
+    exit;
+}
+
+
+foreach ($userNumbers as $number)
+{
+	if ($number < 0)
+	{
+		echo "Корретно посчитать процент отрицательного числа " . $number . " от суммы всех введенных чисел нельзя" . PHP_EOL;
+	}
+	else
+	{
+		$percentage = round(($number/$numbersSum)*100, 2);
+		echo "Число " . $number . " составляет " . $percentage . "% от общей суммы" . PHP_EOL;
+	}
+}
+
+
+
+
+function containsOnlyIntegers($data)
+{
+    if (!$data)
     {
-        return False;
+        return false;
     }
-    foreach ($arr as $arrElement)
+    foreach ($data as $arrayElement)
     {
-        if(!preg_match('/^[-?\d]+$/', $arrElement))
+        if(!preg_match('/^[-]?[\d]+$/', $arrayElement))
         {
-            return False;
+            return false;
         }
     }
-    return True;
+    return true;
 }
