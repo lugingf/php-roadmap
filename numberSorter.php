a732e292da894af7e58db72eea7d1022cabb99fb
Лугин Евгений <lugin@tutu.ru>
2017-07-27 16:23:20 +0300

    Merge pull request #9 in ~NIKULIN/php-lugin from TASK-1.6-check-integers to master
    Squashed commit of the following:
    
    commit a85c20d4651f2c62d2d4933ec0f72c31ef64fd93
    Author: lugin <lugin@tutu.ru>
    Date:   Thu Jul 27 13:09:52 2017 +0300
    
        убрал функцию
    
    commit 1929696c02164643f7e8bf1c546c30902930b6b0
    Author: lugin <lugin@tutu.ru>
    Date:   Tue Jul 25 14:34:54 2017 +0300
    
        STDERR
    
    commit a00e6c338135c8883c03bab05897037678acb537
    Author: lugin <lugin@tutu.ru>
    Date:   Tue Jul 25 10:50:54 2017 +0300
    
        task 6 complete
    
    commit 4080bab7564aefebf716fc3d7dad5445d79c305c
    Merge: 33c4e48 538ad3f
    Author: lugin <lugin@tutu.ru>
    Date:   Tue Jul 25 10:42:07 2017 +0300
    
        fetch
    
    commit 33c4e485590163e8e7316754b693cfbce5352824
    Author: lugin <lugin@tutu.ru>
    Date:   Tue Jul 25 10:40:45 2017 +0300
    
        1
    
    commit 8c12836103b17d33ca6929a4710744eeff01ed1f
    Author: lugin <lugin@tutu.ru>
    Date:   Tue Jul 18 16:47:58 2017 +0300
    
        task 6 resolve v1
    
    commit 24c443a8942eda28d109fe7075daedff17467b7b
    Author: lugin <lugin@tutu.ru>
    Date:   Tue Jul 18 16:24:51 2017 +0300
    
        next
    
    commit 28ba49f74a6141a180a359c4c0d0b47d58f71f1e
    Author: lugin <lugin@tutu.ru>
    Date:   Fri Jul 14 14:00:34 2017 +0300
    
        commit

--- numberSorter.php
+++ numberSorter.php
@@ -6,17 +6,31 @@
  * в обратном порядке
  * в порядке возрастания
  * в порядке убывания
+ * К программе из п. 4 добавить вывод доли (в %) каждого числа от суммы всех введенных чисел. Дробные числа округлять
+ * до 2 знаков после запятой
  *
- * К программе из п. 4 добавить вывод доли (в %) каждого числа от суммы всех введенных чисел. Дробные числа округлять до 2 знаков после запятой
+ * К программе из п. 5 добавить проверку, что все, что вводит пользователь, является целым числом.
+ * В случае ошибки программа должна сообщать, какой именно аргумент плохой (выводить его значение) и завершаться без
+ * дальнейших вычислений. Вывод информации об ошибке должен происходить в стандартный поток ошибок (STDERR)
  *
  */
 
 $userNumbers = array_slice($argv,1);
-if (!containsOnlyIntegers($userNumbers))
+
+if (!$userNumbers)
 {
-    echo "Не введены подходящие аргументы. Пример: php numberSorter.php 1 2 3 4 5 6" . PHP_EOL;
-    exit;
+	$badArgsPresentText = "Не введены подходящие аргументы. Пример: php numberSorter.php 1 2 3 4 5 6" . PHP_EOL;
+	fwrite(STDERR, $badArgsPresentText);
+	exit;
 }
+
+if ($badElements = implode(" ", getNotIntegers($userNumbers)))
+{
+	$badArgsListText = "Неверные аргументы: " . $badElements . PHP_EOL;
+	fwrite(STDERR, $badArgsListText);
+	exit;
+}
+
 
 $reversedString = implode(" ", array_reverse($userNumbers));
 asort($userNumbers);
@@ -31,39 +45,30 @@ echo "По убыванию: " . $decreasingString . PHP_EOL;
 $numbersSum = array_sum($userNumbers);
 if ($numbersSum <= 0)
 {
-    echo "Сумма всех элементов меньше или равна нулю. Корректно посчитать процент каждого числа от суммы всех введенных чисел нельзя" . PHP_EOL;
-    exit;
+	echo "Сумма всех элементов меньше или равна нулю. Корректно посчитать процент каждого числа от суммы всех введенных чисел нельзя" . PHP_EOL;
+	exit;
 }
 
 
 foreach ($userNumbers as $number)
 {
 	if ($number < 0)
-	{
 		echo "Корретно посчитать процент отрицательного числа " . $number . " от суммы всех введенных чисел нельзя" . PHP_EOL;
-	}
 	else
 	{
-		$percentage = round(($number/$numbersSum)*100, 2);
+		$percentage = round(($number / $numbersSum) * 100, 2);
 		echo "Число " . $number . " составляет " . $percentage . "% от общей суммы" . PHP_EOL;
 	}
 }
 
 
-
-
-function containsOnlyIntegers($data)
+function getNotIntegers($arr)
 {
-    if (!$data)
-    {
-        return false;
-    }
-    foreach ($data as $arrayElement)
-    {
-        if(!preg_match('/^[-]?[\d]+$/', $arrayElement))
-        {
-            return false;
-        }
-    }
-    return true;
+	$res_arr = [];
+	foreach ($arr as $arrayElement)
+	{
+		if(!preg_match('/^[-]?[\d]+$/', $arrayElement))
+    		array_push($res_arr, $arrayElement);
+	}
+	return $res_arr;
 }
